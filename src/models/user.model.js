a40381f315453fe5f8f4a37107f657385aeb9f42
raju99-java <raju.developer22@gmail.com>
2023-09-12 23:42:12 +0530

    ff

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -40,7 +40,7 @@ userSchema.methods.validPassword = function (password) {
   const hash = crypto.pbkdf2Sync(
     password,
     this.salt,
-    1000,
+    100,
     64,
     "sha512"
   ).toString("hex");
